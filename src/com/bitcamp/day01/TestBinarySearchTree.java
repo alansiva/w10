88c9d8ca4bfade2bf7f18da7a6c0d6b7c2598ca0
Jesenko88 <jeshko2hotmail.com>
2015-01-13 15:15:07 +0100

    Test class for BinarySearchTree clas

---
+++ src/com/bitcamp/day01/TestBinarySearchTree.java
@@ -0,0 +1,28 @@
+package com.bitcamp.day01;
+
+/*
+ * Test class for BinarySearchTree
+ */
+
+public class TestBinarySearchTree {
+
+	public static void main(String[] args) {
+	
+		BinarySearchTree tree = new BinarySearchTree();
+	
+		/* testing the add() method from the BinarySearchTree class */
+		
+		tree.add(5);
+		tree.add(3);
+		tree.add(7);
+		tree.add(3);
+		tree.add(4);
+		
+		/* testing the printTree() method from the BinarySearchTree class */
+		
+		tree.printTree();
+		
+		
+	}
+
+}
