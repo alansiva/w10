9da58a7c06cd3813a2e9cb4374c35ff31c5ce2ef
Jesenko88 <jeshko2hotmail.com>
2015-01-15 15:18:40 +0100

    pop method corrected

--- src/ba/bitcamp/zadacaBenjo/MyQueue.java
+++ src/ba/bitcamp/zadacaBenjo/MyQueue.java
@@ -9,40 +9,45 @@ import java.util.Stack;
 
 public class MyQueue {
 
-	private Stack<Integer> first  = new Stack<Integer>();
+	private Stack<Integer> first = new Stack<Integer>();
 	private Stack<Integer> second = new Stack<Integer>();
 
 	/**
 	 * Method adds a new element to queue
-	 * @param value to add
+	 * 
+	 * @param value
+	 *            to add
 	 */
 	public void push(int value) {
 		first.add(value);
 	}
 
 	/**
-	 * Method removes the first added element from the queue by 'FIFO'(first in first out) order,
-	 * and returns its value.
+	 * Method removes the first added element from the queue by 'FIFO'(first in
+	 * first out) order, and returns its value.
+	 * 
 	 * @return value of the removed element
 	 */
 	public int pop() {
-		second.push(first.pop());
-
+		while (!first.isEmpty()) {
+			second.push(first.pop());
+		}
 		return second.pop();
 	}
+
 	/**
 	 * 
 	 * @return size of the queue
 	 */
-	public int size(){
+	public int size() {
 		return first.size();
 	}
-	
+
 	/**
 	 * 
 	 * @return first element in the queue
 	 */
-	public int peek(){
+	public int peek() {
 		return first.peek();
 	}
 
